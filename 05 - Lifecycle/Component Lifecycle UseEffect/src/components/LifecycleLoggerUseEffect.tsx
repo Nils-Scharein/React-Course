9c09e317cf44d70d386cc03155baba325234dc09
Nils <nilsscharein@yahoo.de>
2025-08-01 12:37:08 +0200

    finish 05 - Lifecycle

--- 05 - Lifecycle/Component Lifecycle UseEffect/src/components/LifecycleLoggerUseEffect.tsx
+++ 05 - Lifecycle/Component Lifecycle UseEffect/src/components/LifecycleLoggerUseEffect.tsx
@@ -1,16 +1,39 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 
-const LifecycleLoggerUseEffect = () =>
-{
-  useEffect(() => {
+const LifecycleLoggerUseEffect = () => {
 
-  }, []);
 
-  return (
-      <div className="logger-container">
-        <h2>Lifecycle Logger (Function Component)</h2>
+    const [count, setCount] = useState(0);
 
-      </div>
-  )
+    // componentDidMount
+    useEffect(() => {
+        console.log("Component Mounted")
+
+        // ComponentWill Unmount
+        return () => {
+            console.log("Component unmounted")
+        }
+    }, []);
+
+    // componentDidUpdate
+    useEffect(() => {
+        console.log("Vaue changed:", count)
+    }, [count]);
+
+
+    const incrementCount = () => {
+        return setCount((prev) => prev + 1);
+    }
+
+    return (
+        <div className="logger-container">
+            <h2>Lifecycle Logger (Function Component)</h2>
+            <p>Count: {count}</p>
+            <button className="secondary-btn"
+                    onClick={incrementCount}>Increase
+            </button>
+
+        </div>
+    )
 }
 export default LifecycleLoggerUseEffect;
